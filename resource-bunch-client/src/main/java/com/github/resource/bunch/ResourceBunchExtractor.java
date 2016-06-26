f962175f4be708378eb20fd3c968f797d2df8608
miracelwhipp <miracelwhipp@gmx.de>
2016-06-26 18:07:47 +0200

    initial project ready for github

---
+++ resource-bunch-client/src/main/java/com/github/resource/bunch/ResourceBunchExtractor.java
@@ -0,0 +1,85 @@
+package com.github.resource.bunch;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * This utility class provides functionality to extract resource bunches from the classpath.
+ *
+ * @author miracelwhipp
+ */
+public final class ResourceBunchExtractor {
+
+	public static final String DEFAULT_BUNCH_DESCRIPTOR = "resource.bunch.properties";
+	public static final String RESOURCE_SEPARATOR = ",";
+	public static final int BUFFER_SIZE = 128 * 1024;
+
+	private ResourceBunchExtractor() {
+	}
+
+	public static void extract(String bunchName, File targetDirectory) throws IOException {
+
+		extract(DEFAULT_BUNCH_DESCRIPTOR, bunchName, targetDirectory);
+	}
+
+	public static void extract(String descriptorName, String bunchName, File targetDirectory) throws IOException {
+
+		Enumeration<URL> resources = ResourceBunchExtractor.class.getClassLoader().getResources(descriptorName);
+
+		List<String> resourcesToExtract = new ArrayList<>();
+
+		while (resources.hasMoreElements()) {
+
+			URL resource = resources.nextElement();
+
+			Properties properties = new Properties();
+
+			try (InputStream source = resource.openStream()) {
+
+				properties.load(source);
+			}
+
+			String resourceListString = properties.getProperty(bunchName);
+
+			if (resourceListString == null) {
+
+				continue;
+			}
+
+			resourcesToExtract.addAll(Arrays.asList(resourceListString.split(RESOURCE_SEPARATOR)));
+		}
+
+
+		for (String resourceToExtract : resourcesToExtract) {
+
+			File targetFile = new File(targetDirectory, resourceToExtract);
+
+			targetFile.getParentFile().mkdirs();
+
+			try (OutputStream target = new FileOutputStream(targetFile);
+			     InputStream source = ResourceBunchExtractor.class.getClassLoader().
+					     getResourceAsStream(resourceToExtract)) {
+
+				byte[] buffer = new byte[BUFFER_SIZE];
+
+				int length = 0;
+
+				while ((length = source.read(buffer)) > 0) {
+
+					target.write(buffer, 0, length);
+				}
+			}
+		}
+	}
+
+
+}
---
+++ resource-bunch-maven-plugin/src/main/java/com/github/resource/bunch/ResourceBunchMojo.java
@@ -0,0 +1,187 @@
+package com.github.resource.bunch;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * This goal packs the specified resource files to the output directory and lists their names in a properties file.
+ *
+ * @author miracelwhipp
+ */
+@Mojo(name = "create", defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
+public class ResourceBunchMojo extends AbstractMojo {
+
+	public static final int BUFFER_SIZE = 128 * 1024;
+
+	@Parameter(required = true)
+	private Map<String, String> resources;
+
+	@Parameter(defaultValue = "resource.bunch.properties", property = "resource.bunch.descriptor")
+	private String bunchDescriptorResource;
+
+	@Parameter(property = "project.build.outputDirectory", readonly = true)
+	private File outputDirectory;
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+
+		Map<String, List<String>> bunches = new HashMap<>();
+
+		for (Map.Entry<String, String> entry : resources.entrySet()) {
+
+			getLog().debug("found bundle " + entry.getKey());
+
+			List<String> resources = new ArrayList<>();
+
+			bunches.put(entry.getKey(), resources);
+
+			File resource = new File(entry.getValue());
+
+			if (!resource.exists()) {
+
+				getLog().warn("resource does not exist " + entry.getKey() + " (" + resource.getAbsolutePath() + ") - will be ignored.");
+
+				continue;
+			}
+
+			storeBunch(resource, outputDirectory, resources);
+
+		}
+
+		Properties bunchDescriptor = new Properties();
+
+		for (Map.Entry<String, List<String>> entry : bunches.entrySet()) {
+
+			StringBuilder builder = new StringBuilder();
+
+			boolean first = true;
+
+			for (String resourcePart : entry.getValue()) {
+
+				if (first) {
+
+					first = false;
+
+				} else {
+
+					builder.append(",");
+				}
+
+				builder.append(resourcePart);
+			}
+
+			bunchDescriptor.setProperty(entry.getKey(), builder.toString());
+		}
+
+		try (OutputStream target =
+				     new FileOutputStream(new File(outputDirectory, bunchDescriptorResource))) {
+
+			bunchDescriptor.store(target, null);
+
+		} catch (IOException e) {
+
+			throw new MojoFailureException(e.getMessage(), e);
+		}
+	}
+
+	private void storeBunch(File resource, File targetDirectory, List<String> resources)
+			throws MojoFailureException {
+
+		if (!resource.isDirectory()) {
+
+			resources.add(resourceName(copyFile(resource, targetDirectory)));
+
+			return;
+		}
+
+
+		File[] subFiles = resource.listFiles();
+
+		if (subFiles == null) {
+
+			return;
+		}
+
+		for (File subFile : subFiles) {
+
+			storeBunch(subFile, new File(targetDirectory, resource.getName()), resources);
+		}
+
+	}
+
+	private String resourceName(File file) {
+
+		String absoluteFile = file.getAbsolutePath();
+		String absoluteOutputDir = outputDirectory.getAbsolutePath();
+
+		if (!absoluteFile.startsWith(absoluteOutputDir)) {
+
+			throw new IllegalStateException("wrong resource " + absoluteFile + " directory " + absoluteOutputDir);
+		}
+
+		String result = absoluteFile.substring(absoluteOutputDir.length());
+
+		if (result.startsWith("/") || result.startsWith("\\")) {
+
+			result = result.substring(1);
+		}
+
+		result = result.replaceAll("\\\\", "/");
+
+		return result;
+	}
+
+	private static File copyFile(File resource, File targetDirectory) throws MojoFailureException {
+
+		if (!targetDirectory.exists()) {
+
+			if (!targetDirectory.mkdirs()) {
+
+				throw new MojoFailureException("unable to create directory " + targetDirectory.getAbsolutePath());
+			}
+		}
+
+		if (targetDirectory.isFile()) {
+
+			throw new MojoFailureException("directory is a file " + targetDirectory.getAbsolutePath());
+		}
+
+
+		File targetFileName = new File(targetDirectory, resource.getName());
+
+		try (InputStream input = new FileInputStream(resource);
+		     OutputStream target = new FileOutputStream(targetFileName)) {
+
+			byte[] buffer = new byte[BUFFER_SIZE];
+
+			int length = 0;
+
+			while ((length = input.read(buffer)) > 0) {
+
+				target.write(buffer, 0, length);
+			}
+
+		} catch (IOException e) {
+
+			throw new MojoFailureException(e.getMessage(), e);
+		}
+
+		return targetFileName;
+	}
+}
