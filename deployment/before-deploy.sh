31fccebe5e1551f394f25d2c294e4efe58b13b97
miracelwhipp <miracelwhipp@gmx.de>
2018-08-26 22:34:48 +0200

    make scripts executable

--- deployment/before-deploy.sh
+++ deployment/before-deploy.sh
@@ -3,7 +3,8 @@
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 
     openssl aes-256-cbc -K $encrypted_9aa72e404c2e_key -iv $encrypted_9aa72e404c2e_iv -in deployment/codesigning.asc.enc -out deployment/codesigning.asc -d
-    gpg --version
-    gpg2 --version
-    gpg --fast-import deployment/codesigning.asc
+
+    echo "decrypted file"
+
+    gpg2 --fast-import deployment/codesigning.asc
 fi
