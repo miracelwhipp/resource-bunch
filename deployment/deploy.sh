1a311d40a0cf8a2b93a5182954c463bde3c47fdc
miracelwhipp <miracelwhipp@gmx.de>
2018-08-26 19:18:59 +0200

    build debug log

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+
+	echo "calling maven deploy - with settings: "
+	cat deployment/settings.xml
+
     mvn deploy --settings deployment/settings.xml
 fi
