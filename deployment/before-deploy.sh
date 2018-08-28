48c9e9cd9f4900b118dd1e0859323eb51e8689d4
miracelwhipp <miracelwhipp@gmx.de>
2018-08-28 23:46:07 +0200

    more messages

--- deployment/before-deploy.sh
+++ deployment/before-deploy.sh
@@ -2,9 +2,13 @@
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 
-#    openssl aes-256-cbc -K $encrypted_9aa72e404c2e_key -iv $encrypted_9aa72e404c2e_iv -in deployment/codesigning.asc.enc -out deployment/codesigning.asc -d
 	openssl aes-256-cbc -K $encrypted_ab1c605bcedb_key -iv $encrypted_ab1c605bcedb_iv -in deployment/codesigning.asc.enc -out deployment/codesigning.asc -d
 
     gpg2 --fast-import deployment/codesigning.asc
+
+    echo "keys"
     gpg2 --list-keys
+
+    echo "secret keys"
+    gpg2 --list-secret-keys
 fi
