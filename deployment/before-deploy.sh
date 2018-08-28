9940753e35890b86418b62a13188a7201787530b
miracelwhipp <miracelwhipp@gmx.de>
2018-08-28 22:09:24 +0200

    corrected encrypted file

--- deployment/before-deploy.sh
+++ deployment/before-deploy.sh
@@ -2,7 +2,8 @@
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 
-    openssl aes-256-cbc -K $encrypted_9aa72e404c2e_key -iv $encrypted_9aa72e404c2e_iv -in deployment/codesigning.asc.enc -out deployment/codesigning.asc -d
+#    openssl aes-256-cbc -K $encrypted_9aa72e404c2e_key -iv $encrypted_9aa72e404c2e_iv -in deployment/codesigning.asc.enc -out deployment/codesigning.asc -d
+	openssl aes-256-cbc -K $encrypted_ab1c605bcedb_key -iv $encrypted_ab1c605bcedb_iv -in deployment/codesigning.asc.enc -out deployment/codesigning.asc -d
 
     echo "decrypted file"
     head -n3 deployment/codesigning.asc
