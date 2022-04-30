0f5bde1e3f9a2e9213d8c58034f7bd1698e17dcb
miracelwhipp <miracelwhipp@gmx.de>
2022-04-30 10:41:56 +0200

    add github actions ci

--- deployment/before-deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-
-	openssl aes-256-cbc -K $encrypted_ab1c605bcedb_key -iv $encrypted_ab1c605bcedb_iv -in deployment/codesigning.asc.enc -out deployment/codesigning.asc -d
-
-    gpg2 --fast-import --allow-non-selfsigned-uid deployment/codesigning.asc
-fi
